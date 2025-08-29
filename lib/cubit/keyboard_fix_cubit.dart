0766f61b9e890b647d8380cd96bf455481facb08
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-08-30 00:03:42 +0300

    make app support ar , en numbers

--- lib/cubit/keyboard_fix_cubit.dart
+++ lib/cubit/keyboard_fix_cubit.dart
@@ -58,6 +58,17 @@ class KeyboardFixCubit extends Cubit<KeyboardFixState> {
     "A": "ِ",
     "R": "ٌ",
     "Y": "إ",
+    "0": "٠",
+    "1": "١",
+    "2": "٢",
+    "3": "٣",
+    "4": "٤",
+    "5": "٥",
+    "6": "٦",
+    "7": "٧",
+    "8": "٨",
+    "9": "٩",
+
   };
 
   static final Map<String, String> _arToEn = {
